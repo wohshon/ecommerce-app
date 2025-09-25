918443f5d6120609e379e79a85e034c6430a2cda
Woh Shon Phoon <wohshon@gmail.com>
2025-09-25 23:09:08 +0800

    multi arch build

--- build-all.sh
+++ build-all.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 # for quick testing, use latest tag
 cd ./product-service/
-docker build -t docker.io/wohshon/product-service:latest .
-docker push docker.io/wohshon/product-service:latest
+docker buildx build --platform linux/amd64,linux/arm64 -t docker.io/wohshon/product-service:latest --push .
+# docker push docker.io/wohshon/product-service:latest
 cd ../order-service/
-docker build -t docker.io/wohshon/order-service:latest .
-docker push docker.io/wohshon/order-service:latest
+docker buildx build --platform linux/amd64,linux/arm64 -t docker.io/wohshon/order-service:latest --push .
+# docker push docker.io/wohshon/order-service:latest
 cd ../payment-service/
-docker build -t docker.io/wohshon/payment-service:latest .
-docker push docker.io/wohshon/payment-service:latest
+docker buildx build --platform linux/amd64,linux/arm64 -t docker.io/wohshon/payment-service:latest --push .
+# docker push docker.io/wohshon/payment-service:latest
 cd ../frontend/
-docker build -t docker.io/wohshon/frontend:latest .
-docker push docker.io/wohshon/frontend:latest
+docker buildx build --platform linux/amd64,linux/arm64 -t docker.io/wohshon/frontend:latest --push .
+# docker push docker.io/wohshon/frontend:latest
+
