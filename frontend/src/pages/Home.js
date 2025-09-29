c55fd6b7953e7db158ee79ebfae71d76aad26683
Woh Shon Phoon <wohshon@gmail.com>
2025-09-29 22:23:27 +0800

    update check in files

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -70,7 +70,7 @@ function Home() {
 
   return (
     <div style={{ padding: 20, fontFamily: "Arial, sans-serif" }}>
-      <h1>Ecommerce Demo</h1>
+      <h1>Ecommerce Store Demo on NKP</h1>
       <h2>Products</h2>
 
       {products.length === 0 ? (
