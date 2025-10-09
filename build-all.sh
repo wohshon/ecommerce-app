0b169ef5f4dcb644beb48bb63c5856cb09af54d7
ws <wohshon@gmail.com>
2025-10-09 03:36:17 +0000

    change repo

--- build-all.sh
+++ build-all.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # for quick testing, use latest tag
 cd ./product-service/
-docker build -t docker.io/wohshon/product-service:latest .
-docker push docker.io/wohshon/product-service:latest
+docker build -t registry.wskn.local/wohshon/product-service:latest .
+docker push registry.wskn.local/wohshon/product-service:latest
 cd ../order-service/
-docker build -t docker.io/wohshon/order-service:latest .
-docker push docker.io/wohshon/order-service:latest
+docker build -t registry.wskn.local/wohshon/order-service:latest .
+docker push registry.wskn.local/wohshon/order-service:latest
 cd ../payment-service/
-docker build -t docker.io/wohshon/payment-service:latest .
-docker push docker.io/wohshon/payment-service:latest
+docker build -t registry.wskn.local/wohshon/payment-service:latest .
+docker push registry.wskn.local/wohshon/payment-service:latest
 cd ../frontend/
-docker build -t docker.io/wohshon/frontend:latest .
-docker push docker.io/wohshon/frontend:latest
+docker build -t registry.wskn.local/wohshon/frontend:latest .
+docker push registry.wskn.local/wohshon/frontend:latest
 
