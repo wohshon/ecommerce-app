15ccc54be1b4e300808d1c3d11ad2b3a69addf12
ws <wohshon@gmail.com>
2025-10-15 04:00:01 +0000

    phone price change

--- product-service/index.js
+++ product-service/index.js
@@ -17,7 +17,7 @@ console.log(`HOST is ${HOST}`);
 
 const products = [
   { id: 1, name: "Laptop", price: 1699.99, image: "images/laptop.png" },
-  { id: 2, name: "Phone",  price: 799.99,  image: "images/phone.png" },
+  { id: 2, name: "Phone",  price: 899.99,  image: "images/phone.png" },
   // { id: 3, name: "Mug",    price: 9.99,    image: "images/mug.jpg" },
   { id: 3, name: "Headphones", price: 199.99, image: "images/headphones.png" },
   { id: 4, name: "Smartwatch", price: 299.99, image: "images/smartwatch.png" },
