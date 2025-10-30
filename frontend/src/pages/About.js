4178da8da8dd850082b379b42038c9751cd4521a
ws <wohshon@gmail.com>
2025-10-30 11:16:45 +0000

    update about us

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <div>
       <h1>About Our Store</h1>
-      <p>We sell awesome products! - Hosted on Nutanix Kubernetes Platform</p>
+      <p>We sell awesome products! - Hosted on Nutanix Kubernetes Platform v2.16</p>
     </div>
   );
 }
