8881b0a1f68f1afd79579acc673ac98cce3ba375
ws <wohshon@gmail.com>
2025-11-14 06:56:30 +0000

    update repo and home.js

--- build-all.sh
+++ build-all.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 # for quick testing, use latest tag
+# ./build-all.sh docker.io or ./build-all.sh registry.wskn.local
+# docker.io
+# registry.wskn.local
+echo "Building and Pushing all Docker images for ecommerce-app"
+echo "for registry: $1"
 cd ./product-service/
-docker build -t docker.io/wohshon/product-service:latest .
-docker push docker.io/wohshon/product-service:latest
+docker build -t $1/wohshon/product-service:latest .
+docker push $i/wohshon/product-service:latest
 cd ../order-service/
-docker build -t docker.io/wohshon/order-service:latest .
-docker push docker.io/wohshon/order-service:latest
+docker build -t $1/wohshon/order-service:latest .
+docker push $1/wohshon/order-service:latest
 cd ../payment-service/
-docker build -t docker.io/wohshon/payment-service:latest .
-docker push docker.io/wohshon/payment-service:latest
+docker build -t $1/wohshon/payment-service:latest .
+docker push $1/wohshon/payment-service:latest
 cd ../frontend/
-docker build -t docker.io/wohshon/frontend:latest .
-docker push docker.io/wohshon/frontend:latest
+docker build -t $1/wohshon/frontend:latest .
+docker push $1/wohshon/frontend:latest
 
